a3cf60810a7752229130c2146990f65fff7c8a88
jmvanlit <81432358+jmvanlit@users.noreply.github.com>
2026-01-27 07:44:39 +0000

    Deploying to gh-pages from @ jmvanlit/jmvanlit.github.io@5b1210c931dda8bab98300bc36de604000691af0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,7 +389,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/202512-jmvanlit-resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
